eb364861c499285ccfd95b0c08f9a350a1fad75b
itsdfish <itsdfish@gmail.com>
2020-08-18 04:00:55 -0400

    add fitness dependency

--- src/AntColonyOptimization.jl
+++ src/AntColonyOptimization.jl
@@ -1,8 +1,10 @@
 module AntColonyOptimization
     using Parameters, StatsFuns, StatsBase, Statistics, Random, ProgressMeter
+    using Fitness
     import Base.Threads: @threads, nthreads, threadid
     export AntColony, Ant, ColonyState, optimize!, find_path!, select_best_ants
     export initialize, set_pheremones!, compute_probabilities!, set_pheremones!
+
     include("structs.jl")
     include("main.jl")
 end
